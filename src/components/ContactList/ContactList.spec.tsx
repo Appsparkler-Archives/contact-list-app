bc5e7f09303b0a9928c8be96e5497a920bf254e9
Aakash Shah <subs@appsparkler.com>
2023-05-01 15:27:51 +0530

    chore: add key to list iteration

--- src/components/ContactList/ContactList.spec.tsx
+++ src/components/ContactList/ContactList.spec.tsx
@@ -9,6 +9,7 @@ import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
 
 interface IContact {
+  id: string;
   name: string;
   imgUrl: string;
   company?: string;
@@ -16,6 +17,7 @@ interface IContact {
 
 const contacts: IContact[] = [
   {
+    id: "7cb1248c-983a-4207-a59f-40bfa1fae564",
     name: "Ali Connors",
     imgUrl:
       "https://plus.unsplash.com/premium_photo-1671641797679-3b680a7d2109?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZSUyMHBpY3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60",
@@ -27,7 +29,7 @@ function ContactList({ contacts }: { contacts: IContact[] }) {
   return (
     <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
       {contacts.map((contact) => (
-        <>
+        <React.Fragment key={contact.id}>
           <ListItem alignItems="flex-start">
             <ListItemAvatar>
               <Avatar alt={"Remy Sharp"} src={contact.imgUrl} />
@@ -49,7 +51,7 @@ function ContactList({ contacts }: { contacts: IContact[] }) {
             />
           </ListItem>
           <Divider variant="inset" component="li" />
-        </>
+        </React.Fragment>
       ))}
     </List>
   );

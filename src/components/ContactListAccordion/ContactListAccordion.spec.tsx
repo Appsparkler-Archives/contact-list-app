13a578955a9c0214203160945999d2fe98665840
Aakash Shah <subs@appsparkler.com>
2023-05-07 16:27:17 +0530

    chore: wip contact list accordion

--- src/components/ContactListAccordion/ContactListAccordion.spec.tsx
+++ src/components/ContactListAccordion/ContactListAccordion.spec.tsx
@@ -1,9 +1,24 @@
 import { render } from "@testing-library/react";
 import { ContactListAccordion } from "./ContactListAccordion";
+import { IContactFormData } from "types";
+import { contactsData } from "data";
 
 describe("ContactListAccordion Snapshot Tests", () => {
   test("snapshot 1", () => {
-    const { container } = render(<ContactListAccordion />);
+    const { container } = render(
+      <ContactListAccordion
+        contacts={contactsData.variant1}
+        onEdit={function (editedContactData: IContactFormData): void {
+          throw new Error("Function not implemented.");
+        }}
+        onDelete={function (deletedContactId: string): void {
+          throw new Error("Function not implemented.");
+        }}
+        onView={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
+    );
     expect(container).toMatchSnapshot();
   });
 });

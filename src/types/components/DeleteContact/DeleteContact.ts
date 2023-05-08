a009999cfc48ad27948c7ebe8ae7d82f05e5dd52
Aakash Shah <subs@appsparkler.com>
2023-05-08 12:41:20 +0530

    chore: use only props, remove unnecessary FC type

--- src/types/components/DeleteContact/DeleteContact.ts
+++ src/types/components/DeleteContact/DeleteContact.ts
@@ -1,13 +1,13 @@
 import React from "react";
 import { IContactFormData } from "types/data";
 
-export type TDeleteContactDialogFC = React.FC<{
+export type TDeleteContactDialogProps = {
   open: boolean;
   contact: IContactFormData;
   onClose: () => void;
   onDelete: (contactIdToDelete: string) => void;
   onCancel: React.MouseEventHandler<HTMLButtonElement>;
-}>;
+};
 
 export type TDeleteContactModalFC = React.FC<{
   contact: IContactFormData;

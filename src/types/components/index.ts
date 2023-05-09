0e09da48ccc330cf23458985f1b36029c4b3a00e
Aakash Shah <subs@appsparkler.com>
2023-05-09 08:55:43 +0530

    chore: create contact form

--- src/types/components/index.ts
+++ src/types/components/index.ts
@@ -5,3 +5,4 @@ export * from "./ContactForm";
 export * from "./DeleteContact";
 export * from "./FullScreenModalTransition";
 export * from "./ViewContactModal";
+export * from "./CreateContactForm";

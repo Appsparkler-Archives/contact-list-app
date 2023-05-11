a70bdf711b4d1866460ec0304c801308245a9cb7
Aakash Shah <subs@appsparkler.com>
2023-05-11 17:00:55 +0530

    test: wip

--- src/App_Connected.spec.tsx
+++ src/App_Connected.spec.tsx
@@ -1,7 +1,8 @@
-import { render } from "@testing-library/react";
 import { ConnectedApp } from "App_Connected";
-import { store } from "app/store";
+import { store, RootState } from "app/store";
 import { Provider } from "react-redux";
+import { render } from "@testing-library/react";
+import configureStore from "redux-mock-store";
 
 const AppRenderer = () => (
   <Provider store={store}>
@@ -9,9 +10,48 @@ const AppRenderer = () => (
   </Provider>
 );
 
+jest.mock("app/hooks");
+
 describe("App - Snapshot Tests", () => {
   it("should render App component", () => {
     const { container } = render(<AppRenderer />);
     expect(container).toMatchSnapshot();
+  });
+});
+
+// Mock the app/hooks module
+jest.mock("app/hooks", () => ({
+  useAppDispatch: jest.fn(),
+  useAppSelector: jest.fn(),
+}));
+
+describe("ConnectedApp", () => {
+  const mockStore = configureStore<RootState>();
+  let $store: typeof store;
+
+  beforeEach(() => {
+    $store = mockStore({
+      contacts: [],
+    });
+
+    const rootState = $store.getState();
+
+    // Mock the return values for useAppSelector and useAppDispatch
+    jest.mock("app/hooks", () => ({
+      useAppDispatch: jest.fn(),
+      useAppSelector: jest.fn((selector) => selector(rootState)),
+    }));
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("renders App component with contacts and event handlers", () => {
+    render(
+      <Provider store={$store}>
+        <ConnectedApp />
+      </Provider>
+    );
   });
 });

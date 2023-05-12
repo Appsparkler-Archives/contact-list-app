f1224df98ef299905e65e5cc99301cc3077e3772
Aakash Shah <subs@appsparkler.com>
2023-05-12 15:46:57 +0530

    test: contacts/delete action is dispatched

--- src/App_Connected.spec.tsx
+++ src/App_Connected.spec.tsx
@@ -2,21 +2,21 @@ import { cleanup, fireEvent, render, screen } from "@testing-library/react";
 import { ConnectedApp } from "App_Connected";
 import { RootState } from "app/store";
 import { contactsData } from "data";
+import { act } from "react-dom/test-utils";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
-import { changeTextField, clickOnFirstCreateButton } from "test-utils";
-// import { useAppDispatch, useAppSelector } from "app/hooks";
-
-const selectorFunction = (selector: (state: RootState) => any) =>
-  selector(state);
+import {
+  changeTextField,
+  clickOnFirstCreateButton,
+  clickOnYesDeleteButton,
+} from "test-utils";
 
 jest.useFakeTimers();
-// jest.mock("app/hooks", () => {
-//   return {
-//     useAppDispatch: jest.fn().mockImplementation(() => jest.fn),
-//     useAppSelector: jest.fn().mockImplementation(() => selectorFunction),
-//   };
-// });
+
+beforeEach(() => {
+  store.clearActions();
+});
+
 afterEach(cleanup);
 
 const state: RootState = {
@@ -44,10 +44,20 @@ describe("ConnectedApp - Snapshot Tests", () => {
     clickOnAddContactButton();
     expect(baseElement).toMatchSnapshot();
   });
+
+  it("should match snapshot after clicking on accordion trigger", () => {
+    const { baseElement } = render(
+      <Provider store={store}>
+        <ConnectedApp />
+      </Provider>
+    );
+    clickOnContactAccordion();
+    expect(baseElement).toMatchSnapshot();
+  });
 });
 
 describe("ConnectedApp - Functional Tests", () => {
-  it("create functionality", () => {
+  it("should dispatch contacts/create action", () => {
     render(
       <Provider store={store}>
         <ConnectedApp />
@@ -62,8 +72,34 @@ describe("ConnectedApp - Functional Tests", () => {
     const actionsAfter = store.getActions();
     expect(actionsAfter[0].type).toBe("contacts/create");
   });
+
+  it("should dispatch contacts/delete action", () => {
+    expect(store.getActions().length).toBe(0);
+    render(
+      <Provider store={store}>
+        <ConnectedApp />
+      </Provider>
+    );
+    clickOnContactAccordion();
+    clickOnDeleteIconButton();
+    clickOnYesDeleteButton();
+    act(() => {
+      jest.runAllTimers();
+    });
+    const actionsAfter = store.getActions();
+
+    expect(actionsAfter[0].type).toBe("contacts/delete");
+  });
 });
 
 function clickOnAddContactButton() {
   fireEvent.click(screen.getByText(/add new contact/i));
+}
+
+function clickOnContactAccordion() {
+  fireEvent.click(screen.getByText(/Ali Connors/i));
+}
+
+function clickOnDeleteIconButton() {
+  fireEvent.click(screen.getAllByLabelText(/delete/i)[0]);
 }

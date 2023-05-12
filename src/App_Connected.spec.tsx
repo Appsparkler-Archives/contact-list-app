078fdd4fc1f1c0157ce110f3cc1dc3f8337c31cd
Aakash Shah <subs@appsparkler.com>
2023-05-12 15:30:16 +0530

    chore: update snapshots

--- src/App_Connected.spec.tsx
+++ src/App_Connected.spec.tsx
@@ -1,13 +1,26 @@
-import { act, fireEvent, render, screen } from "@testing-library/react";
+import { cleanup, fireEvent, render, screen } from "@testing-library/react";
 import { ConnectedApp } from "App_Connected";
 import { RootState } from "app/store";
+import { contactsData } from "data";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
+import { changeTextField, clickOnFirstCreateButton } from "test-utils";
+// import { useAppDispatch, useAppSelector } from "app/hooks";
+
+const selectorFunction = (selector: (state: RootState) => any) =>
+  selector(state);
 
 jest.useFakeTimers();
+// jest.mock("app/hooks", () => {
+//   return {
+//     useAppDispatch: jest.fn().mockImplementation(() => jest.fn),
+//     useAppSelector: jest.fn().mockImplementation(() => selectorFunction),
+//   };
+// });
+afterEach(cleanup);
 
 const state: RootState = {
-  contacts: [],
+  contacts: contactsData.variant1,
 };
 const getStore = configureStore([]);
 const store = getStore(state);
@@ -29,21 +42,25 @@ describe("ConnectedApp - Snapshot Tests", () => {
       </Provider>
     );
     clickOnAddContactButton();
-    // act(() => {
-    //   jest.runAllTimers();
-    // });
     expect(baseElement).toMatchSnapshot();
   });
 });
 
 describe("ConnectedApp - Functional Tests", () => {
-  it("should", () => {
+  it("create functionality", () => {
     render(
       <Provider store={store}>
         <ConnectedApp />
       </Provider>
     );
     clickOnAddContactButton();
+    changeTextField(/name/i, "Jhon");
+    changeTextField(/mobile/i, "999");
+    const actionsBefore = store.getActions();
+    expect(actionsBefore.length).toBe(0);
+    clickOnFirstCreateButton();
+    const actionsAfter = store.getActions();
+    expect(actionsAfter[0].type).toBe("contacts/create");
   });
 });
 

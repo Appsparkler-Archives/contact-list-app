a42373aed98631344e6f2cf7405443e9ad7d1612
Aakash Shah <subs@appsparkler.com>
2023-05-12 15:02:58 +0530

    test:snapshot tests for app-connected

--- src/App_Connected.spec.tsx
+++ src/App_Connected.spec.tsx
@@ -1,57 +1,52 @@
+import { act, fireEvent, render, screen } from "@testing-library/react";
 import { ConnectedApp } from "App_Connected";
-import { store, RootState } from "app/store";
+import { RootState } from "app/store";
 import { Provider } from "react-redux";
-import { render } from "@testing-library/react";
 import configureStore from "redux-mock-store";
 
-const AppRenderer = () => (
-  <Provider store={store}>
-    <ConnectedApp />;
-  </Provider>
-);
+jest.useFakeTimers();
 
-jest.mock("app/hooks");
+const state: RootState = {
+  contacts: [],
+};
+const getStore = configureStore([]);
+const store = getStore(state);
 
-describe("App - Snapshot Tests", () => {
-  it("should render App component", () => {
-    const { container } = render(<AppRenderer />);
-    expect(container).toMatchSnapshot();
-  });
-});
-
-// Mock the app/hooks module
-jest.mock("app/hooks", () => ({
-  useAppDispatch: jest.fn(),
-  useAppSelector: jest.fn(),
-}));
-
-describe("ConnectedApp", () => {
-  const mockStore = configureStore<RootState>();
-  let $store: typeof store;
-
-  beforeEach(() => {
-    $store = mockStore({
-      contacts: [],
-    });
-
-    const rootState = $store.getState();
-
-    // Mock the return values for useAppSelector and useAppDispatch
-    jest.mock("app/hooks", () => ({
-      useAppDispatch: jest.fn(),
-      useAppSelector: jest.fn((selector) => selector(rootState)),
-    }));
-  });
-
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
-  test("renders App component with contacts and event handlers", () => {
-    render(
-      <Provider store={$store}>
+describe("ConnectedApp - Snapshot Tests", () => {
+  it("should match snapshot", () => {
+    const { container } = render(
+      <Provider store={store}>
         <ConnectedApp />
       </Provider>
     );
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should match snapshot after opening add new contact modal", () => {
+    const { baseElement } = render(
+      <Provider store={store}>
+        <ConnectedApp />
+      </Provider>
+    );
+    clickOnAddContactButton();
+    // act(() => {
+    //   jest.runAllTimers();
+    // });
+    expect(baseElement).toMatchSnapshot();
   });
 });
+
+describe("ConnectedApp - Functional Tests", () => {
+  it("should", () => {
+    render(
+      <Provider store={store}>
+        <ConnectedApp />
+      </Provider>
+    );
+    clickOnAddContactButton();
+  });
+});
+
+function clickOnAddContactButton() {
+  fireEvent.click(screen.getByText(/add new contact/i));
+}
